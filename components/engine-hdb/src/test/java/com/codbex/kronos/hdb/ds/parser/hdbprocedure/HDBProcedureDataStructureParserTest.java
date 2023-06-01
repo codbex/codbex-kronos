967b9949d2b85e19a82fee8f538a723edc4791b6
delchev <delchevn@gmail.com>
2023-06-01 17:21:39 +0300

    procedure test migrated

--- components/engine-hdb/src/test/java/com/codbex/kronos/hdb/ds/parser/hdbprocedure/HDBProcedureDataStructureParserTest.java
+++ components/engine-hdb/src/test/java/com/codbex/kronos/hdb/ds/parser/hdbprocedure/HDBProcedureDataStructureParserTest.java
@@ -11,59 +11,68 @@
  */
 package com.codbex.kronos.hdb.ds.parser.hdbprocedure;
 
-//import com.codbex.kronos.engine.hdb.api.DataStructuresException;
-//import com.codbex.kronos.exceptions.ArtifactParserException;
-//import com.codbex.kronos.hdb.ds.artefacts.HDBProcedureSynchronizationArtefactType;
-//import com.codbex.kronos.hdb.ds.parser.TestContentProvider;
-//import com.codbex.kronos.hdb.ds.parser.TestProcedureParser;
-//import com.codbex.kronos.hdb.ds.synchronizer.DataStructuresSynchronizer;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//import org.mockito.Mockito;
-//
-//import java.io.IOException;
-//
-//import static org.junit.Assert.assertEquals;
-//import static org.junit.Assert.assertNotNull;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.doNothing;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.io.IOException;
+
+import javax.transaction.Transactional;
+
+import org.eclipse.dirigible.repository.api.IRepository;
+import org.eclipse.dirigible.repository.api.IRepositoryStructure;
+import org.eclipse.dirigible.repository.api.IResource;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import com.codbex.kronos.engine.hdb.api.DataStructuresException;
+import com.codbex.kronos.engine.hdb.domain.HDBProcedure;
+import com.codbex.kronos.engine.hdb.parser.HDBDataStructureModelFactory;
+import com.codbex.kronos.exceptions.ArtifactParserException;
+import com.codbex.kronos.hdb.ds.repository.TestRepositoryMigrator;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ComponentScan(basePackages = { "org.eclipse.dirigible.components", "com.codbex.kronos"})
+@EntityScan(value = {"org.eclipse.dirigible.components", "com.codbex.kronos"})
+@Transactional
 public class HDBProcedureDataStructureParserTest {
+	
+  @Autowired
+  private TestRepositoryMigrator migrator;
+  
+  @Autowired
+  private IRepository repository;
 
-//  TestContentProvider contentProvider = new TestContentProvider();
-//  TestProcedureParser procedureParser = new TestProcedureParser();
-//  private HDBProcedureDataStructureParser parser;
-//
-//  @Before
-//  public void setup() {
-//    DataStructuresSynchronizer dataStructuresSynchronizerMock = Mockito.mock(DataStructuresSynchronizer.class);
-//    doNothing().when(dataStructuresSynchronizerMock).applyArtefactState(any(), any(), any(), any(), any());
-//    HDBProcedureLogger hdbProcedureLogger = Mockito.mock(HDBProcedureLogger.class);
-//    doNothing().when(hdbProcedureLogger).logError(any(), any(), any());
-//
-//    HDBProcedureSynchronizationArtefactType hdbProcedureSynchronizationArtefactType = new HDBProcedureSynchronizationArtefactType();
-//    parser = new HDBProcedureDataStructureParser(dataStructuresSynchronizerMock, hdbProcedureSynchronizationArtefactType, hdbProcedureLogger);
-//  }
-//
-//  @Test
-//  public void checkModel() throws IOException, DataStructuresException, ArtifactParserException {
-//    String location = "/OrderProcedure.hdbprocedure";
-//    String content = contentProvider.getTestContent(location);
-//
-//    var model = procedureParser.parseProcedure(parser, location, content);
-//    assertEquals("Unexpected hdbprocedure schema.", "MYSCHEMA", model.getSchema());
-//    assertEquals("Unexpected hdbprocedure name.", "hdb_view::OrderProcedure", model.getName());
-//    assertEquals("Unexpected hdbprocedure content.", content, model.getRawContent());
-//    assertEquals("Unexpected hdbprocedure raw content.", content, model.getRawContent());
-//    assertEquals("Unexpected hdbprocedure location.", location, model.getLocation());
-//    assertEquals("Unexpected hdbprocedure type.", "HDBPROCEDURE", model.getType());
-//    assertEquals("Unexpected hdbprocedure dependencies.", 0, model.getDependencies().size());
-//    assertNotNull("Null value for hdbprocedure createdAt", model.getCreatedAt());
-//    assertNotNull("Null value for hdbprocedure createdBy", model.getCreatedBy());
-//    assertEquals("Cant access data structure model class",
-//        "class com.codbex.kronos.hdb.ds.model.hdbprocedure.DataStructureHDBProcedureModel", parser.getDataStructureClass().toString());
-//  }
+  @Test
+  public void checkModel() throws IOException, DataStructuresException, ArtifactParserException {
+	final String location = "procedure/OrderProcedure.hdbprocedure";
+	migrator.migrate(location);
+	IResource loadedResource = repository.getResource(IRepositoryStructure.PATH_REGISTRY_PUBLIC + IRepository.SEPARATOR + location);
+    String fileContent = new String(loadedResource.getContent());
+	HDBProcedure parsedModel = HDBDataStructureModelFactory.parseProcedure("procedure/OrderProcedure.hdbprocedure", fileContent);
+    assertEquals("MYSCHEMA", parsedModel.getSchema(), "Unexpected hdbprocedure schema.");
+    assertEquals("hdb_view::OrderProcedure", parsedModel.getName(), "Unexpected hdbprocedure name.");
+    assertEquals("procedure/OrderProcedure.hdbprocedure", parsedModel.getLocation(), "Unexpected hdbprocedure location.");
+    assertEquals("HDBPROCEDURE", parsedModel.getType(), "Unexpected hdbprocedure type.");
+//    assertEquals("Unexpected hdbprocedure dependencies.", 0, parsedModel.getDependencies().size());
+    assertNull(parsedModel.getCreatedAt(), "Null value for hdbprocedure createdAt");
+    assertNull(parsedModel.getCreatedBy(), "Null value for hdbprocedure createdBy");
+  }
 
+  /**
+   * The Class TestConfiguration.
+   */
+  @EnableJpaRepositories(basePackages = "com.codbex.kronos")
+  @SpringBootApplication(scanBasePackages = {"com.codbex.kronos", "org.eclipse.dirigible.components"})
+  @EnableScheduling
+  static class TestConfiguration {
+  }
 
 }
