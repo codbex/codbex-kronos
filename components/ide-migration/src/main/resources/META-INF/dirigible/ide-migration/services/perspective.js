f05e357792b94fd73698ac5e0b43077d654d72ac
StanZGenchev <stan.z.genchev@gmail.com>
2023-08-14 12:52:43 +0300

    Added new migration perspective
    Signed-off-by: StanZGenchev <stan.z.genchev@gmail.com>

--- components/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/services/perspective.js
+++ components/ide-migration/src/main/resources/META-INF/dirigible/ide-migration/services/perspective.js
@@ -1,20 +1,19 @@
 /*
- * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
+ * SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and Eclipse Dirigible contributors
  * SPDX-License-Identifier: EPL-2.0
  */
 const perspectiveData = {
     id: "migration",
-    name: "SAP HANA XS Classic Migration",
-    link: "../ide-migration/migration-perspective.html",
-    order: "820",
-    image: "exchange", // deprecated
+    name: "HANA XS Classic Migration",
+    link: "../ide-migration/perspective.html",
+    order: "850",
     icon: "../ide-migration/images/migration.svg",
 };
 if (typeof exports !== 'undefined') {
