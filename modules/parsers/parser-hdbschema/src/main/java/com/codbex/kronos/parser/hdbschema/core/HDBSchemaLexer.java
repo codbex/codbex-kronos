3a988c9ce926fe78bf095d1a16770157c597f43d
ThuF <jordan.p.pavlov@gmail.com>
2023-09-26 06:55:39 +0000

    version set to 1.0.0-SNAPSHOT for development

--- modules/parsers/parser-hdbschema/src/main/java/com/codbex/kronos/parser/hdbschema/core/HDBSchemaLexer.java
+++ modules/parsers/parser-hdbschema/src/main/java/com/codbex/kronos/parser/hdbschema/core/HDBSchemaLexer.java
@@ -1,14 +1,3 @@
-/*
- * Copyright (c) 2022-2023 codbex or an codbex affiliate company and contributors
- *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v20.html
- *
- * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
- * SPDX-License-Identifier: EPL-2.0
- */
 // Generated from com/codbex/kronos/parser/hdbschema/core/HDBSchema.g4 by ANTLR 4.13.0
 package com.codbex.kronos.parser.hdbschema.core;
 import org.antlr.v4.runtime.Lexer;
