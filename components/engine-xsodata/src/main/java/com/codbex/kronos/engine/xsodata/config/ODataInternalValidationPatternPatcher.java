11b6a1375abb6d820a8c3dcaa7e741f818a9ba38
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-04-19 16:37:49 +0300

    olingo 3.1.4 - delete reflection

--- components/engine-xsodata/src/main/java/com/codbex/kronos/engine/xsodata/config/ODataInternalValidationPatternPatcher.java
+++ components/engine-xsodata/src/main/java/com/codbex/kronos/engine/xsodata/config/ODataInternalValidationPatternPatcher.java
@@ -11,14 +11,8 @@
  */
 package com.codbex.kronos.engine.xsodata.config;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.Arrays;
 import java.util.regex.Pattern;
 import org.apache.olingo.odata2.core.edm.provider.EdmNamedImplProv;
-import sun.misc.Unsafe;
 
 class ODataNamesValidationPatternPatcher {
 
@@ -32,33 +26,6 @@ class ODataNamesValidationPatternPatcher {
       .compile("\\A[_\\p{L}\\p{Nl}][_.\\p{L}\\p{Nl}\\p{Nd}\\p{Mn}\\p{Mc}\\p{Pc}\\p{Cf}]{0,}\\Z");
 
   void patch() throws ReflectiveOperationException {
-    setFinalStatic(EdmNamedImplProv.class.getDeclaredField(NAME_OF_THE_PROPERTY_NAME_PATTERN_FIELD),
-        ODATA_PROPERTY_NAME_PATTERN);
-  }
-
-  private static void setFinalStatic(Field field, Object newValue)
-      throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException, SecurityException {
-
-    field.setAccessible(true); // NOSONAR
-
-    Field modifiersField = getModifiersField();
-    modifiersField.setAccessible(true); // NOSONAR
-    modifiersField.setInt(field, (field.getModifiers() & ~Modifier.FINAL)); // NOSONAR
-
-    final Field unsafeField = Unsafe.class.getDeclaredField("theUnsafe");
-    unsafeField.setAccessible(true);
-    final Unsafe unsafe = (Unsafe) unsafeField.get(null);
-
-    //field.set(null, newValue); // NOSONAR
-    unsafe.putObject(field.getDeclaringClass(), unsafe.staticFieldOffset(field), newValue);
-  }
-
-  private static Field getModifiersField()
-      throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-    Method getDeclaredFields0 = Class.class.getDeclaredMethod("getDeclaredFields0", boolean.class);
-    getDeclaredFields0.setAccessible(true); // NOSONAR
-    Field[] allDeclaredFields = (Field[]) getDeclaredFields0.invoke(Field.class, false);
-    return Arrays.stream(allDeclaredFields).filter(f -> "modifiers".equals(f.getName())).findFirst()
-        .orElseThrow(() -> new IllegalStateException("Could not get modifiers field"));
+    EdmNamedImplProv.setValidNamePattern(ODATA_PROPERTY_NAME_PATTERN);
   }
 }
