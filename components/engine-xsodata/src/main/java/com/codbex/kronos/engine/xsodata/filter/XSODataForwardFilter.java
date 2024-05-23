bc52a68e1850c326de60a132af7c8f8cdb0f604d
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-05-23 14:38:59 +0200

    Apply codbex formatter (#575)

--- components/engine-xsodata/src/main/java/com/codbex/kronos/engine/xsodata/filter/XSODataForwardFilter.java
+++ components/engine-xsodata/src/main/java/com/codbex/kronos/engine/xsodata/filter/XSODataForwardFilter.java
@@ -1,9 +1,8 @@
 /*
- * Copyright (c) 2022-2023 codbex or an codbex affiliate company and contributors
+ * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
  * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
@@ -28,53 +27,52 @@ import org.springframework.stereotype.Component;
 @Component
 public class XSODataForwardFilter implements Filter {
 
-  /**
-   * Inits the.
-   *
-   * @param filterConfig the filter config
-   * @throws ServletException the servlet exception
-   */
-  @Override
-  public void init(FilterConfig filterConfig) throws ServletException {
-    //
-  }
-
-  /**
-   * Do filter.
-   *
-   * @param request the request
-   * @param response the response
-   * @param chain the chain
-   * @throws IOException Signals that an I/O exception has occurred.
-   * @throws ServletException the servlet exception
-   */
-  @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-    //only on production case
-    if (httpServletRequest.getHeader("Dirigible-Editor") == null) {
-      String uri = httpServletRequest.getRequestURI();
-      int index = uri.indexOf(".xsodata");
-      if (index > 0) {
-        String parameters = "";
-        if (uri.length() > index + (".xsodata".length() - 1)) {
-          parameters = uri.substring(index + ".xsodata".length());
-        }
-        String path = "/odata/v2" + parameters;
-        RequestDispatcher dispatcher = request.getRequestDispatcher(path);
-        dispatcher.forward(request, response);
-      }
+    /**
+     * Inits the.
+     *
+     * @param filterConfig the filter config
+     * @throws ServletException the servlet exception
+     */
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        //
     }
-    chain.doFilter(request, response);
-  }
 
-  /**
-   * Destroy.
-   */
-  @Override
-  public void destroy() {
-    //
-  }
+    /**
+     * Do filter.
+     *
+     * @param request the request
+     * @param response the response
+     * @param chain the chain
+     * @throws IOException Signals that an I/O exception has occurred.
+     * @throws ServletException the servlet exception
+     */
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        // only on production case
+        if (httpServletRequest.getHeader("Dirigible-Editor") == null) {
+            String uri = httpServletRequest.getRequestURI();
+            int index = uri.indexOf(".xsodata");
+            if (index > 0) {
+                String parameters = "";
+                if (uri.length() > index + (".xsodata".length() - 1)) {
+                    parameters = uri.substring(index + ".xsodata".length());
+                }
+                String path = "/odata/v2" + parameters;
+                RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+                dispatcher.forward(request, response);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+
+    /**
+     * Destroy.
+     */
+    @Override
+    public void destroy() {
+        //
+    }
 
 }
