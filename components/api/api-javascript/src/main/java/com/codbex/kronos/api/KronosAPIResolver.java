8bd9639124a02e01fde4581df9a516b5b569d6f8
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-06-21 16:00:28 +0300

    add Kronos ABAP API for http println

--- components/api/api-javascript/src/main/java/com/codbex/kronos/api/KronosAPIResolver.java
+++ components/api/api-javascript/src/main/java/com/codbex/kronos/api/KronosAPIResolver.java
@@ -1,7 +1,16 @@
+/*
+ * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
+ *
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
 package com.codbex.kronos.api;
 
 import java.nio.file.Path;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
@@ -12,33 +21,34 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-class KronosAPIResolver implements ExternalModuleResolver {
+public class KronosAPIResolver implements ExternalModuleResolver {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KronosAPIResolver.class);
 
-    private static final List<String> PURE_ESM_MODULES = List.of("sdk/abap");
+    private static final String API_MODULE_PREFIX = "sdk/";
 
-    private static final Pattern DIRIGIBLE_CORE_MODULE_SIGNATURE_PATTERN = Pattern.compile("(sdk)/(\\w+)(?:/(.+))?"); // e.g.
+    private static final String ABAP_API_MODULE_PREFIX = API_MODULE_PREFIX + "abap";
+    private static final Pattern API_MODULE_PATTERN = Pattern.compile(API_MODULE_PREFIX + "(\\w+)(?:/(.+))?");
+    private static final Pattern ABAP_RUN_FILE_PATTERN = Pattern.compile("^(?:.*/)?([^/]+)/src/run\\.mjs$");
+
+    // must match the folder name where the KRONOS apis are located
+    // src/main/resources/META-INF/dirigible/kronos
+    // this folder is copied to the Dirigible registry, and it is resolved on runtime
+    private static final String KRONOS_DIRIGIBLE_PROJECT_FOLDER = "kronos";
 
     private final JavascriptSourceProvider sourceProvider;
 
-    /**
-     * Instantiates a new dirigible esm module resolver.
-     *
-     * @param sourceProvider the source provider
-     */
     KronosAPIResolver(JavascriptSourceProvider sourceProvider) {
         this.sourceProvider = sourceProvider;
     }
 
     @Override
     public boolean isResolvable(String moduleToResolve) {
-        return isPureEsmModule(moduleToResolve);
+        return isKronosModule(moduleToResolve);
     }
 
-    private boolean isPureEsmModule(String module) {
-        return PURE_ESM_MODULES.stream()
-                               .anyMatch(module::startsWith);
+    private boolean isKronosModule(String module) {
+        return module.startsWith(ABAP_API_MODULE_PREFIX);
     }
 
     /**
@@ -50,24 +60,29 @@ class KronosAPIResolver implements ExternalModuleResolver {
     @Override
     public Path resolve(String moduleToResolve) {
         LOGGER.debug("Resolving actual path for [{}]", moduleToResolve);
-        Matcher modulePathMatcher = DIRIGIBLE_CORE_MODULE_SIGNATURE_PATTERN.matcher(moduleToResolve);
-        if (!modulePathMatcher.matches()) {
-            throw new RuntimeException("Found invalid core modules with path [" + moduleToResolve + "] since it doesn't match pattern: "
-                    + DIRIGIBLE_CORE_MODULE_SIGNATURE_PATTERN);
+
+        Matcher apiModuleMatcher = API_MODULE_PATTERN.matcher(moduleToResolve);
+        boolean apiModule = apiModuleMatcher.matches();
+        if (apiModule) {
+            String moduleDir = apiModuleMatcher.group(1);
+            String moduleFile = apiModuleMatcher.group(2);
+
+            boolean hasModuleFile = !StringUtils.isEmpty(moduleFile);
+            String file = hasModuleFile ? moduleFile + ".mjs" : "index.mjs";
+            return resolvePath(KRONOS_DIRIGIBLE_PROJECT_FOLDER, "dist", "esm", moduleDir, file);
         }
 
-        String moduleDir = modulePathMatcher.group(2);
-        String moduleFile = modulePathMatcher.group(3);
-
-        boolean hasModuleFile = !StringUtils.isEmpty(moduleFile);
-
-        Path resolvedPath = sourceProvider.getAbsoluteProjectPath("kronos")
-                                          .resolve("dist")
-                                          .resolve("esm")
-                                          .resolve(moduleDir)
-                                          .resolve(hasModuleFile ? moduleFile + ".mjs" : "index.mjs");
-
-        LOGGER.debug("Path for [{}] is resolved to [{}]", moduleToResolve, resolvedPath);
-        return resolvedPath;
+        throw new IllegalStateException("Couldn't resolve Kronos API module with path [" + moduleToResolve
+                + "] since it doesn't match patterns: [" + ABAP_RUN_FILE_PATTERN + "] and [" + API_MODULE_PATTERN + "]");
     }
+
+    private Path resolvePath(String projectPath, String... folders) {
+        Path path = sourceProvider.getAbsoluteProjectPath(projectPath);
+        for (String folder : folders) {
+            path = path.resolve(folder);
+        }
+        LOGGER.debug("Path resolved to [{}]", path);
+        return path;
+    }
+
 }
--- components/api/api-javascript/src/main/resources/META-INF/dirigible/kronos/build-source.sh
+++ components/api/api-javascript/src/main/resources/META-INF/dirigible/kronos/build-source.sh
@@ -3,16 +3,28 @@ set -e
 
 # clean
 rm -rf ./dist
+rm -rf ./node_modules
 
 # install node modules
-rm -rf ./node_modules
 npm install
 
+mkdir dist
+
+npm install
+
+npm run transpile
+
+npm run lint
+
+# Find all .mjs files in the current directory and its subdirectories,
+# and replaces all occurrences of %23 with # in those files.
+find . -name '*.mjs' -print0 | xargs -0 sed -i '' 's/%23/#/g'
+
 # build esm
-esbuild $(find . -iname '*.ts' -not -iname '*.d.ts') '--out-extension:.js=.mjs' --sourcemap=inline --outdir=dist/esm --format=esm --target=es2022
+esbuild $(find ./src/js -iname '*.ts' -not -iname '*.d.ts') '--out-extension:.js=.mjs' --sourcemap=inline --outdir=dist/esm --format=esm --target=es2022
 
 # build cjs
-esbuild $(find . -iname '*.ts' -not -iname '*.d.ts') --sourcemap=inline --outdir=dist/cjs --format=cjs --target=es2022
+esbuild $(find ./src/js -iname '*.ts' -not -iname '*.d.ts') --sourcemap=inline --outdir=dist/cjs --format=cjs --target=es2022
 
 # build dts
 tsc --emitDeclarationOnly --outDir dist/dts
