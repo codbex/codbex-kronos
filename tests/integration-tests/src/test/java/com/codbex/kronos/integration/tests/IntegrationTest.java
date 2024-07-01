fa39e16302d3f2a1da3bbd83c267c255e33e3f6f
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-07-01 10:23:22 +0300

    change app build to fix the tests

--- tests/integration-tests/src/test/java/com/codbex/kronos/integration/tests/IntegrationTest.java
+++ tests/integration-tests/src/test/java/com/codbex/kronos/integration/tests/IntegrationTest.java
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2024 Eclipse Dirigible contributors
+ * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
  *
  * All rights reserved. This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: Eclipse Dirigible contributors SPDX-License-Identifier: EPL-2.0
+ * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
+ * SPDX-License-Identifier: EPL-2.0
  */
 package com.codbex.kronos.integration.tests;
 
