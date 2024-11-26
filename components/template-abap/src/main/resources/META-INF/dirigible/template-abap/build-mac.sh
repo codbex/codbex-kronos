dd069fc0b4e3ccc27bcef48089ea2c3d0e9acc82
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-11-26 09:43:36 +0200

    remove abap lint and add logs

--- components/template-abap/src/main/resources/META-INF/dirigible/template-abap/build-mac.sh
+++ components/template-abap/src/main/resources/META-INF/dirigible/template-abap/build-mac.sh
@@ -1,6 +1,14 @@
 # fail the whole script if any command bellow fails
 set -e
 
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') --- $1"
+}
+log "Executing Linux build script..."
+
+script_path=$(realpath "$0")
+log "Script path: $script_path"
+
 rm -rf dist
 rm -rf node_modules
 
@@ -9,17 +17,22 @@ mkdir dist
 npm install
 
 # check package.json for more details
+log "Transpiling the ABAP code..."
 npm run transpile
 
-npm run lint
+# disable abaplint
+# npm run lint
 
 # Find all .mjs files in the current directory and its subdirectories,
 # and replaces all occurrences of %23 with # in those files.
 find . -name '*.mjs' -print0 | xargs -0 sed -i '' 's/%23/#/g'
 
+log "Packaging the code..."
 esbuild src/run.mjs --tsconfig=./tsconfig.json --bundle --outdir=dist --format=esm --target=es2022 \
     --external:tls --external:net --external:util --external:crypto --external:zlib \
     --external:http --external:https --external:fs --external:path --external:url \
     --external:sdk \
     --inject:./src/lib/polyfills/buffer.js --inject:./src/lib/polyfills/process.js \
     --out-extension:.js=.mjs
+
+log "Linux build script $script_path completed!"
