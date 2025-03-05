2a2d4489fbeafea7d6005ef445a89a62c1929e85
delchev <delchevn@gmail.com>
2025-03-05 18:57:45 +0200

    temporary old branding

---
+++ branding/src/main/resources/META-INF/dirigible/ide-branding/branding.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+const brandingInfo = {
+    name: 'codbex',
+    brand: 'codbex',
+    brandUrl: 'https://www.codbex.com/',
+    icons: {
+        faviconIco: '/services/web/ide-branding/images/favicon.ico',
+        favicon32: '/services/web/ide-branding/images/favicon-32x32.png',
+        favicon16: '/services/web/ide-branding/images/favicon-16x16.png',
+    },
+    logo: '/services/web/ide-branding/images/logo.svg',
+};
