f79cfa4a748fbcc65eafa637ffc4968cf2e623c2
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-06 11:30:35 +0200

    Adapt tests to latest framework changes in Dirigible

--- tests/integration-tests/src/test/java/com/codbex/kronos/integration/tests/ui/TestCfgs.java
+++
@@ -1,9 +0,0 @@
-package com.codbex.kronos.integration.tests.ui;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-
-@ComponentScan(basePackages = "org.eclipse.dirigible")
-@TestConfiguration
-class TestCfgs {
-}
--- tests/integration-tests/src/test/java/org/eclipse/dirigible/tests/TestConfigurations.java
+++ tests/integration-tests/src/test/java/org/eclipse/dirigible/tests/TestConfigurations.java
@@ -12,9 +12,11 @@ package org.eclipse.dirigible.tests;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 
+@ComponentScan(basePackages = "org.eclipse.dirigible")
 @TestConfiguration
 public class TestConfigurations {
 
