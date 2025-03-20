9760c7e6cb74b9c312fe6e2e9bc3215945974793
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-20 14:25:17 +0200

    fix-terminal

--- application/src/main/java/com/codbex/kronos/AppLifecycleLoggingListener.java
+++ application/src/main/java/com/codbex/kronos/AppLifecycleLoggingListener.java
@@ -1,39 +1,56 @@
 /*
- * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
+ * Copyright (c) 2025 Eclipse Dirigible contributors
  *
  * All rights reserved. This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
- * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
- * SPDX-License-Identifier: EPL-2.0
+ * SPDX-FileCopyrightText: Eclipse Dirigible contributors SPDX-License-Identifier: EPL-2.0
  */
 package com.codbex.kronos;
 
+import org.eclipse.dirigible.components.base.ApplicationListenersOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextClosedEvent;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
-@Order(Ordered.LOWEST_PRECEDENCE)
+@Order(ApplicationListenersOrder.APP_LYFECYCLE_LOGGING_LISTENER)
 @Component
-class AppLifecycleLoggingListener implements ApplicationListener<ApplicationEvent> {
+class AppLifecycleLoggingListener implements ApplicationListener<ApplicationEvent>, DisposableBean {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AppLifecycleLoggingListener.class);
 
+    private long createdContextAt;
+
     @Override
     public void onApplicationEvent(ApplicationEvent event) {
-        if (event instanceof ApplicationReadyEvent) {
-            LOGGER.info("------------------------ Kronos started ------------------------");
+        if (event instanceof ApplicationStartedEvent) {
+            createdContextAt = System.currentTimeMillis();
+            log("Application is initializing");
         }
-        if (event instanceof ContextClosedEvent) {
-            LOGGER.info("------------------------ Kronos stopped ------------------------");
+
+        if (event instanceof ApplicationReadyEvent) {
+            log("Application has started");
+            long currentTime = System.currentTimeMillis();
+            log("Start time: [{}] milliseconds. Init time: [{}] milliseconds", (currentTime - KronosApplication.getStartedAt()),
+                    (currentTime - createdContextAt));
         }
     }
 
+    private void log(String messageFormat, Object... argument) {
+        LOGGER.info("------- " + messageFormat + " -------", argument);
+    }
+
+    @Override
+    public void destroy() {
+        log("Application stopped");
+        log("Application started at [{}] and has been running for [{}] milliseconds", KronosApplication.getStartedAt(),
+                (System.currentTimeMillis() - KronosApplication.getStartedAt()));
+    }
 }
