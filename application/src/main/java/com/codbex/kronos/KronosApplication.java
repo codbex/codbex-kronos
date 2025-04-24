389a80a6acfd5e68b05b4023697d4a2f80616ca5
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-04-24 15:27:32 +0300

    use common files from dirigible

--- application/src/main/java/com/codbex/kronos/KronosApplication.java
+++ application/src/main/java/com/codbex/kronos/KronosApplication.java
@@ -10,6 +10,7 @@
  */
 package com.codbex.kronos;
 
+import org.apache.camel.opentelemetry.starter.CamelOpenTelemetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -17,12 +18,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerA
 import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "com.codbex.kronos")
+@EnableTransactionManagement
+@CamelOpenTelemetry
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class,
         HibernateJpaAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
 @ComponentScan({"org.eclipse.dirigible", "com.codbex.kronos"})
